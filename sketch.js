c15b81062be3042339f6dcd179248d8bfa829d87
AndyW4ndgerannt <92064035+AndyW4ndgerannt@users.noreply.github.com>
2022-03-26 04:13:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,12 +11,12 @@ function draw() {
     for (let i = 0; i < 80; i++) {
       translate(
         sin(frameCount * 0.001 + j) * 100,
-        sin(frameCount * 0.002 + j) * 100,
+        sin(frameCount * 0.001 + j) * 100,
         i * 0.1
       );
       rotateZ(frameCount * 0.002);
       push();
-      sphere(32, 16, 8, 4, 2, 1);
+      sphere(5, 7, 8, 4, 2, 1);
       pop();
     }
     pop();
